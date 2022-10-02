18092fee436a9cfa31afd3374e025c61131f2776
Sushant Nyachhyon <sushantnyachhyon@gmail.com>
2022-10-02 09:20:02 +0545

    base dotfiles

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,28 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'rmehri01/onenord.nvim'
+    use 'nvim-lualine/lualine.nvim'
+    use 'kyazdani42/nvim-web-devicons'
+    use 'nvim-lua/plenary.nvim' -- Common utilities
+    use 'onsails/lspkind-nvim' -- vscode-like pictograms
+    use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
+    use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
+    use 'hrsh7th/nvim-cmp' -- Completion
+    use 'neovim/nvim-lspconfig'
+    use 'glepnir/lspsaga.nvim'
+    use 'L3MON4D3/LuaSnip'
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    }
+    use 'windwp/nvim-autopairs'
+    use 'windwp/nvim-ts-autotag'
+    use 'nvim-telescope/telescope.nvim'
+    use 'nvim-telescope/telescope-file-browser.nvim'
+    use 'akinsho/nvim-bufferline.lua'
+    use 'norcalli/nvim-colorizer.lua'
+    use 'jose-elias-alvarez/null-ls.nvim'
+    use 'MunifTanjim/prettier.nvim'
+end)
